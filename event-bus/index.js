6149a5e4412be7371ea2d31c8e22a67b3b5fe8d7
DavidMax <davidjamesmax@gmail.com>
2021-11-26 14:50:32 -0800

    Clarify comments in event-bus code

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
+// create new express app
 const app = express();
 
-
+// Use express built in body parser to handle incoming JSON
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// call cors module middleware that
+// handles cross origin issues by setting special header
 app.use(cors());
 
+// Request handler to listen for incoming events & broadcast them back to all services
 app.post('/events', (req, res) => {
+    // Grab event which is request body
     const event = req.body;
-
+    // Make POST requests to all other services containing the event
     axios.post('http://localhost:4000/events', event).catch(err => console.log(err.message));
     axios.post('http://localhost:4001/events', event).catch(err => console.log(err.message));
     axios.post('http://localhost:4002/events', event).catch(err => console.log(err.message));
-
+    // when calls are done respond to original request with status
     res.send({ status: 'OK'});
 })
 
-app.listen(4005, (console.log('Listening on port 4005')));
+app.listen(4005, () => { console.log('Listening on port 4005') });
